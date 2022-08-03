60fa6189b8ba4febe18a1b56d04323c2ee3b58a9
Tom Scott <tscott@stratanetworks.com>
2022-08-03 15:11:58 -0600

    now broadcasting chat to all connected clients

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,10 @@ use tokio::{
 async fn main() {
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
 
-    let (tx, _rx) = broadcast::channel::<String>(10);
+    let (tx, _rx) = broadcast::channel(10);
 
     loop {
-        let (mut socket, _addr) = listener.accept().await.unwrap();
+        let (mut socket, addr) = listener.accept().await.unwrap();
 
         let tx = tx.clone();
         let mut rx = tx.subscribe();
@@ -23,17 +23,22 @@ async fn main() {
             let mut line = String::new();
 
             loop {
-                let bytes_read = reader.read_line(&mut line).await.unwrap();
-                if bytes_read == 0 {
-                    break;
+                tokio::select! {
+                    result = reader.read_line(&mut line) => {
+                        if result.unwrap() == 0 {
+                            break;
+                        }
+                        tx.send((line.clone(), addr)).unwrap();
+                        line.clear();
+                    }
+                    result = rx.recv() => {
+                        let (msg, other_addr) = result.unwrap();
+
+                        if addr != other_addr {
+                            writer.write_all(msg.as_bytes()).await.unwrap();
+                        }
+                    }
                 }
-
-                tx.send(line.clone()).unwrap();
-
-                let msg = rx.recv().await.unwrap();
-
-                writer.write_all(msg.as_bytes()).await.unwrap();
-                line.clear();
             }
         });
     }
